7e45471afdc7116b863c4ae678ae8f8571bfe8c3
Proozz321 <luffyrei523@gmail.com>
2026-05-30 23:29:45 +0100

    GroupBY, Join, SelectMany and Skip/Take

---
+++ Exercices/Join/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;           // LINQ básico (Where, Select, OrderBy...)
+using System.Collections.Generic; // List, IEnumerable, Dictionary...
+using System.Text.RegularExpressions;
+
+
+namespace Join
+{
+    class Program
+    {
+        record Produto(int num, string Item, int ID);
+        record Categoria(int ID, string Tipo);
+        static void Main(string[] args)
+        {
+         var produtos = new List<Produto>
+        {
+            new Produto(1, "Laptop", 1),
+            new Produto(2, "Camiseta", 2),
+            new Produto(3, "Smartphone", 1),
+            new Produto(4, "Calça", 2),
+            new Produto(5, "Fone", 1)
+        };
+
+        var categorias = new List<Categoria>
+        {
+            new Categoria(1, "Eletrônicos"),
+            new Categoria(2, "Roupas")
+        };
+
+        var resultado = produtos.Join(categorias,p => p.ID,c => c.ID,(p, c) => new 
+                {
+                Produto = p.Item,
+                Categoria = c.Tipo
+                }
+        );
+        foreach(var res in resultado)
+        {
+            Console.WriteLine(res);
+        }
+
+        var eletronicos = resultado.Where(r => r.Categoria == "Eletrônicos").Select(r => r.Produto);
+
+        Console.WriteLine("\n=== Só Eletrônicos ===");
+        foreach (var p in eletronicos)
+            Console.WriteLine(p);
+        }
+    }
+}
---
+++ Exercices/SelectMany/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;           // LINQ básico (Where, Select, OrderBy...)
+using System.Collections.Generic; // List, IEnumerable, Dictionary...
+using System.Text.RegularExpressions;
+
+
+namespace SelectMany
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var turmas = new List<List<string>>
+            {
+                new List<string> { "Ana", "Bruno", "Carlos" },
+                new List<string> { "Amanda", "Diego", "Eduardo" },
+                new List<string> { "Fernanda", "Alice", "Gabriel" }
+            };
+
+            var todos = turmas.SelectMany(t => t).ToList();
+            var cont = todos.Count();
+            var cmA = todos.Where(a => a.StartsWith("A"));
+
+             Console.WriteLine("ALUNOOO...................");
+            foreach(var turma in todos)
+            {
+                Console.WriteLine(turma);
+                
+            }
+            Console.WriteLine("TURMAS...................");
+            Console.WriteLine(cont);
+            Console.WriteLine("ALUNOS COM A......................");
+            foreach(var turma in cmA)
+            {
+                Console.WriteLine(turma);
+                
+            }
+
+        }
+    }
+}
