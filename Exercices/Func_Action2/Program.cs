fa8f8247953b8ad9e5b795740a9e5ffe4549b0e0
Proozz321 <luffyrei523@gmail.com>
2026-06-05 23:37:01 +0100

    func_action

---
+++ Exercices/Func_Action2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Func_Action2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Validador v = new Validador();
+            v.Validar("Ma", valor => valor.Length >= 3,
+            () => Console.WriteLine("Dado válido"),
+            () => Console.WriteLine("Dado inválido"));
+        }
+    }
+}
